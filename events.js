6b55ed695d903f8c31c31ec2a4322aba99e7cec2
Bobby Thomas <30844492+AIR-FRAME@users.noreply.github.com>
2018-06-13 14:06:07 -0700

    Update events.js
    Added multiple event feature

--- events.js
+++ events.js
@@ -1,8 +1,21 @@
 var Events	= function(){};
-Events.clientChange = null;
-Events.trigger = function(eventName){
-	Events.clientChange();
+
+Events._events = [];
+Events.bind = function(eventName, callBack){
+	var evnt = {
+		"eventname": eventName,
+		"callback": callBack
+	}	
+	Events._events.push(evnt);
 }
+
+Events.trigger = function(eventName){	
+	var eventObject = Events._events.find((it) => { return it.eventname === eventName; });
+	if(eventObject){
+		eventObject.callback();
+	}
+}
+
 if( typeof module !== "undefined" && ('exports' in module)){
 	module.exports	= Events;
 }
