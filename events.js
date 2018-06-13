d76dc7bf95b4d4605e618f873c4113221109b20e
Bobby Thomas <30844492+AIR-FRAME@users.noreply.github.com>
2018-06-13 14:34:45 -0700

    Update events.js

--- events.js
+++ events.js
@@ -2,20 +2,16 @@ var Events	= function(){};
 
 Events._events = [];
 Events.bind = function(eventName, callBack){
-	var evnt = {
-		"eventname": eventName,
-		"callback": callBack
-	}	
-	Events._events.push(evnt);
+   Events._events.push({ "eventname": eventName, "callback": callBack } );
 }
 
 Events.trigger = function(eventName){	
-	var eventObject = Events._events.find((it) => { return it.eventname === eventName; });
-	if(eventObject){
-		eventObject.callback();
-	}
+   var eventObject = Events._events.find((it) => { return it.eventname === eventName; });
+   if(eventObject){
+      eventObject.callback();
+   }
 }
 
 if( typeof module !== "undefined" && ('exports' in module)){
-	module.exports	= Events;
+   module.exports	= Events;
 }
